a62dadb07361376e57a9a79781efd650f08aff7c
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-10-09 05:46:09 -0300

    Add concrete implementations and EnableErrorHandler annotation
    - Include dependencies in pom
    - Add the EnableErrorHandler annotation to enable error handler configuration
    - Add the error handler concrete implementation class
    - Add configuration class that creates the error handler

---
+++ src/main/java/com/bellotapps/utils/error_handler/EnableErrorHandler.java
@@ -0,0 +1,45 @@
+package com.bellotapps.utils.error_handler;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.*;
+
+/**
+ * Enables the error handling system.
+ *
+ * @author Juan Marcos Bellini
+ * @see ErrorHandler
+ * @see ExceptionHandler
+ * @see ExceptionHandlerObject
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Import({ErrorHandlerCreationConfigurer.class,})
+public @interface EnableErrorHandler {
+
+    /**
+     * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation declarations e.g.:
+     * {@code @EnableErrorHandler("org.my.pkg")} instead of {@code @EnableErrorHandler(basePackages="org.my.pkg")}.
+     */
+    @AliasFor("basePackages")
+    String[] value() default {};
+
+    /**
+     * Base packages to scan for annotated {@link ExceptionHandler}.
+     * {@link #value()} is an alias for (and mutually exclusive with) this
+     * attribute. Use {@link #basePackageClasses()} for a type-safe alternative to String-based package names.
+     */
+    @AliasFor("value")
+    String[] basePackages() default {};
+
+    /**
+     * Type-safe alternative to {@link #basePackages()}
+     * for specifying the packages to scan for annotated {@link ExceptionHandler}.
+     * The package of each class specified will be scanned.
+     * Consider creating a special no-op marker class or interface in
+     * each package that serves no purpose other than being referenced by this attribute.
+     */
+    Class<?>[] basePackageClasses() default {};
+}
